2cabba2fe25b339859091284b679af7e8457e952
David Santos <contato.dvdsantos@gmail.com>
2020-09-26 17:39:39 -0300

    bad request vazio insomnia

--- src/database/migrations/1600794447126-CreateUsers.ts
+++ src/database/migrations/1600794447126-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1600794447126 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
