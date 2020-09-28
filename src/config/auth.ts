a8f238e6367f7c06046874bf19a58cf4b678442b
David Santos <contato.dvdsantos@gmail.com>
2020-09-28 02:22:47 -0300

    users authenticated

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'd36400e24c451c7541fb4af51fa3716c',
+        expiresIn: '1d',
+    },
+};
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -20,7 +20,7 @@ usersRouter.post('/', async (request, response) => {
 
         return response.json(user);
     } catch (err) {
-        return response.status(400).json({ error: err.mesage });
+        return response.status(400).json({ error: err.message });
     }
 });
 
